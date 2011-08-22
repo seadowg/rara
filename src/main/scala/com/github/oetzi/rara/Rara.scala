559dc3f2d625c70c3c467aeb7da796b8af106b12
Callum Stott <oetzi101@gmail.com>
2011-08-22 19:54:57 +0100

    Add implicit type conversion for Rint

--- src/main/scala/com/github/oetzi/rara/Rara.scala
+++ src/main/scala/com/github/oetzi/rara/Rara.scala
@@ -2,6 +2,7 @@ package com.github.oetzi.rara {
 	object Rara {
 		implicit def any2Robj(obj : Any) : Robj = new Robj(obj)
 		implicit def string2Rstring(obj : String) : Rstring = new Rstring(obj)
+		implicit def int2Rint(obj : Int) : Rint = new Rint(obj)
 	}
 
 	class Robj(obj : Any) {
